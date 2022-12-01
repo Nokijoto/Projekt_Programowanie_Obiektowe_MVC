1a9820d368f2581f87c876e479275fd762cd3b0f
Rafał Wątroba <rafalwatroba@protonmail.com>
2022-12-01 10:39:15 +0100

    Dodanie Widoku Car

--- Controllers/CarListController.cs
+++ Controllers/CarListController.cs
@@ -1,6 +1,18 @@
-﻿namespace Projekt_MVC.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projekt_MVC.Controllers
 {
-    public class CarListController
+    public class CarListController : Controller
     {
+        private readonly ILogger<CarListController> _logger;
+
+        public CarListController(ILogger<CarListController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
